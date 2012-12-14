0b4903afadab4328bf8b44454f645b95fb858995
Amil Khanzada <amilkh@github.com>
2012-12-13 22:22:00 -0800

    added direct scripts to test send_openbts_sms() in freeswitch

--- setup_fs.py
+++ setup_fs.py
@@ -15,6 +15,8 @@ setup(name="libvbts",
                                           'freeswitch/VBTS_New_User.py',
                                           'freeswitch/VBTS_Send_SMS.py',
                                           'freeswitch/VBTS_Send_Empty_SMS.py',
+                                          'freeswitch/VBTS_Send_SMS_Direct.py',
+                                          'freeswitch/VBTS_Send_Empty_SMS_Direct.py',
                                           'freeswitch/VBTS_DB_Get.py',
                                           'freeswitch/VBTS_DB_Set.py'])
         ]
