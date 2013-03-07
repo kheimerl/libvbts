73d2a7ad3bda8454335f0dd8f4ff6078e867272f
Kurtis Heimerl <kheimerl@cs.berkeley.edu>
2013-03-06 21:46:13 -0800

    Some util broadcast SMS stuff

---
+++ util/SIPClient.py
@@ -0,0 +1,88 @@
+from twisted.internet import reactor
+from twisted.protocols import sip
+import random
+import logging
+import string
+import sqlite3
+import traceback
+import time
+
+class Client(sip.Base):
+    
+    def __init__(self, sub_reg, sender_ip, sender_port):
+        sip.Base.__init__(self)
+        self.log = logging.getLogger('BM.SIPClient.Client')
+        self.ast_db = sqlite3.connect(sub_reg)
+        self.ip = sender_ip
+        self.port = int(sender_port)
+        self.log.info('Starting...')
+        self.started = False
+        self.cseq = 1
+
+    def __start(self):
+        self.started = True
+        reactor.listenUDP(self.port, self)
+
+    def __gen_string(self, length):
+        return ''.join(random.choice(string.letters) for i in xrange(length))
+
+    def __get_ip(self, target):
+        cur = self.ast_db.cursor()
+        cur.execute('SELECT ipaddr FROM SIP_BUDDIES where name=?', (target,))
+        line = cur.fetchone()
+        res = None
+        if (line):
+            res = line[0]
+        cur.close()
+        return res
+
+    def __get_port(self, target):
+        cur = self.ast_db.cursor()
+        cur.execute('SELECT port FROM SIP_BUDDIES where name=?', (target,))
+        line = cur.fetchone()
+        res = None
+        if (line):
+            res = line[0]
+        cur.close()
+        return res
+
+    #sends a SIMPLE message to smqueue
+    def send_message(self, target, body, return_num):
+        if (not self.started):
+            self.__start()
+        if (not return_num):
+            return_num = "000"
+        target_ip = self.__get_ip(target)
+        target_port = self.__get_port(target)
+        if (target_ip and target_port):
+            r = sip.Request('MESSAGE', sip.URL(target_ip, 
+                                               port=target_port, 
+                                               username=target))
+            r.body = body
+            r.addHeader('via', sip.Via(self.ip).toString())
+            r.addHeader('from', "%d <sip:%d@%s:%s>;tag=%s" % (return_num, return_num,
+                                                           self.ip, self.port, self.__gen_string(6)))
+            r.addHeader('to', "%s <sip:%s@%s>" % (target, target, 
+                                                  target_ip))
+            r.addHeader('call-id', (random.randint(100000, 10000000)))
+            r.addHeader('cseq', "18 MESSAGE")
+            r.addHeader('content-type', "text/plain")
+            r.addHeader('content-length', len(r.body))
+            self.sendMessage(sip.URL(target_ip,
+                                     port=target_port), r)
+            self.log.info("Sent SIP message to %s: %s" % (target, body))
+        else:
+            self.log.warn("Unable to send message to %s: %s" % (target, body))
+
+    def broadcast_message(self, body, return_num):
+        cur = self.ast_db.cursor()
+        cur.execute('SELECT name FROM SIP_BUDDIES')
+        self.log.info("Broadcasting %s" % body)
+        for name in cur:
+            try:
+                self.send_message(name[0], body, return_num)
+                time.sleep(1)
+            except:
+                self.log.warn("Unable to broadcast to %s" % name[0])
+                self.log.warn("%s" % traceback.format_exc())
+                pass
---
+++ util/send_sms.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import SIPClient
+import getopt
+import sys
+import logging
+from twisted.internet import reactor
+
+def usage():
+    print ("Stuff goes here")
+    print ("-t target | --target=TARGET_IMSI")
+    print ("-r return | --return=RETURN NUMBER, must be a number. Defaults to config")
+    print ("-m message | --message=MESSAGE WE WANT TO SEND")
+    print ("-l log_level | --log_level=LOG LEVEL")
+    print ("-d subscriber_reg | --subscriber_reg=SUBSCRIBER_REG LOCATION")
+    exit(1)
+
+opts, args = getopt.getopt(sys.argv[1:],
+                           "t:m:r:l:d:", ["target=", "message=", "return=", "log_level=", "subscriber_reg="])
+log_level = "DEBUG"
+log_loc = "/tmp/libvbts_util.log"
+sub_reg = "/var/lib/asterisk/sqlite3/sqlite3.db"
+message=None
+target=None
+ret = None
+
+for o,a in opts:
+    if o in ("-l", "--log_level="):
+        log_level = a
+    elif o in ("-d", "--subscriber_reg="):
+        sub_reg = a
+    elif o in ("-t", "--target="):
+        target = a
+    elif o in ("-m", "--message="):
+        message = a
+    elif o in ("-r", "--return="):
+        ret = int(a)
+    else:
+        usage()
+
+if not (message or target):
+    usage()
+
+if (log_level == "DEBUG"):
+    log_level = logging.DEBUG
+elif(log_level == "INFO"):
+    log_level = logging.INFO
+elif(log_level == "WARNING"):
+    log_level = logging.WARNING
+elif(log_level == "ERROR"):
+    log_level = logging.ERROR
+elif(log_level == "CRITICAL"):
+    log_level = logging.CRITICAL
+else:
+    print ("Invalid logging level")
+    usage()
+
+logging.basicConfig(filename=log_loc, level=log_level, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+log = logging.getLogger("send_sms")
+
+client = SIPClient.Client(sub_reg, "127.0.0.1", "5065")
+
+reactor.callWhenRunning(client.send_message,target,message,ret)
+reactor.callWhenRunning(reactor.stop)
+# Run the server's main loop
+reactor.run()
